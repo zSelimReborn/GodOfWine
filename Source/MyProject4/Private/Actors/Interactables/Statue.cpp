bbebfe3c7327316d28f5491f1c1c0080b9e4ed32
Pasquale Giaccio <pasquale.giaccio1998@gmail.com>
2023-02-08 22:57:17 +0100

    Created simple interactable statue as last objective

---
+++ Source/MyProject4/Private/Actors/Interactables/Statue.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Interactables/Statue.h"
+
+void AStatue::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DisableInteraction();
+}
+
+void AStatue::Interaction(AActor* InteractionInstigator)
+{
+	if (!CanInteract())
+	{
+		return;	
+	}
+	
+	UE_LOG(LogTemp, Display, TEXT("AStatue::Interaction to implement."));
+	MarkAsCompleted();
+	DisableInteraction();
+	HideInteractWidget();
+	OnObjectiveCompleted.Broadcast(this);
+}
+
+bool AStatue::CanInteract() const
+{
+	return Super::CanInteract();
+}
+
+void AStatue::StartInteraction(AActor* InteractionInstigator)
+{
+	Interaction(InteractionInstigator);
+}
+
+void AStatue::EndInteraction(AActor* InteractionInstigator)
+{
+	Super::EndInteraction(InteractionInstigator);
+}
+
+FString AStatue::GetObjectiveTitle() const
+{
+	return ObjectiveTitle;
+}
+
+FText AStatue::GetObjectiveDescription() const
+{
+	return ObjectiveDescription;
+}
+
+void AStatue::ActivateObjective()
+{
+	EnableInteraction();
+}
+
+bool AStatue::IsObjectiveCompleted()
+{
+	return bIsObjectiveCompleted;
+}
+
+void AStatue::MarkAsCompleted()
+{
+	bIsObjectiveCompleted = true;
+}
---
+++ Source/MyProject4/Public/Actors/Interactables/Statue.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Interactables/InteractableBase.h"
+#include "Interfaces/Objective.h"
+#include "Statue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT4_API AStatue : public AInteractableBase, public IObjective
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	void Interaction(AActor* InteractionInstigator);
+
+// Interactable interface
+public:
+	virtual bool CanInteract() const override;
+	virtual void StartInteraction(AActor* InteractionInstigator) override;
+	virtual void EndInteraction(AActor* InteractionInstigator) override;
+
+// Objective interface
+public:
+	virtual FString GetObjectiveTitle() const override;
+	virtual FText GetObjectiveDescription() const override;
+	virtual void ActivateObjective() override;
+	virtual bool IsObjectiveCompleted() override;
+	virtual void MarkAsCompleted() override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category="Objective")
+	FString ObjectiveTitle;
+
+	UPROPERTY(EditAnywhere, Category="Objective")
+	FText ObjectiveDescription;
+
+	UPROPERTY(Transient)
+	bool bIsObjectiveCompleted = false;
+};
